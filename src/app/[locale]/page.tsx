240f98ede2d8a1effa90280550341c4a0cde1bb9
Urs Schmidt <urs.schmidt@ts.berlin>
2024-04-07 20:28:57 +0200

    feat: basic setup i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import { AfterSection } from "@/components/afterSection";
 import { ContactSection } from "@/components/contactSection";
 import { OpeningSection } from "@/components/openingSection";
@@ -6,9 +7,11 @@ import { SocialSection } from "@/components/socialSection";
 import { Hero } from "@components/hero";
 
 export default function Home() {
+  const t = useTranslations("Index");
   return (
     <main className="flex min-h-screen w-full items-center gap-8 justify-between bg-udh_green px-4 sm:px-16 pb-16">
       <PageWrapper>
+        <h1>{t("title")}</h1>
         <Hero />
         <OpeningSection />
         <AfterSection />
