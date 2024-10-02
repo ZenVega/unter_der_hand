612a4457435d00b9c460b2f9d690d40260f0adbe
ZenVega <ufogreeks@gmail.com>
2024-10-02 16:29:11 +0200

    smooth scroll toop button stransition

--- src/components/pageWrapper/index.tsx
+++ src/components/pageWrapper/index.tsx
@@ -14,24 +14,26 @@ const PageWrapper = ({ children }: props) => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setShowScrollTop(!heroInViewRef.current); // Show button when #hero is not in view
+      setShowScrollTop(!heroInViewRef.current); 
     };
 
-    window.addEventListener("scroll", handleScroll); // Listen to scroll events
+    window.addEventListener("scroll", handleScroll); 
 
     return () => {
-      window.removeEventListener("scroll", handleScroll); // Clean up on unmount
+      window.removeEventListener("scroll", handleScroll); 
     };
   }, [heroInViewRef]);
 
   return (
     <div className="max-w-5xl m-auto">
-      <a
-      href="#hero"
-      className={`fixed bottom-8 z-20 right-8 flex min-w-8 min-h-4 justify-center gap-2 bg-udh_yellow text-udh_dark_green border border-2 border-udh_dark_green px-2 rounded-lg transition-opacity duration-500 ${showScrollTop? "opacity-100" : "opacity-0"}`}
-      >
-        <Icon name="chevron-up" color={colors["udh_dark_green"]} />
-      </a>
+        <a
+          href="#hero"
+          className={`fixed bottom-8 z-20 right-8 flex min-w-8 max-h-6 justify-center gap-2 bg-udh_yellow text-udh_dark_green border border-2 border-udh_dark_green px-2 rounded-lg transition-opacity duration-300 ${
+            showScrollTop ? "opacity-100 " : "opacity-0"
+          }`}
+        >
+          <span className="text-xl font-bold">^</span>
+        </a>
     {children}</div>);
 };
 
